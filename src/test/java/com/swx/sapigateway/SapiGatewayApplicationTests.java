c7742259852ba66f9a3d265fb4fdbf38054212ef
swxswxer <a913685201@163.com>
2024-08-14 02:11:03 +0800

    commit

---
+++ src/test/java/com/swx/sapigateway/SapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swx.sapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
